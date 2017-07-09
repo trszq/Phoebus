2711874d8d34e883064b66855eb1d1e732f85407
Phoebus Zhang <trszhangqing@live.cn>
2017-07-09 22:19:53 +0800

    Day3 v2

---
+++ Day3/func1.py
@@ -0,0 +1,18 @@
+# Author: Zhang Qing
+
+#函数
+def func1():
+    "test"
+    print('in the func1')
+    return 0
+
+#过程
+def func2():
+    '''test2'''
+    print('in the func2')
+
+
+func1()
+func2()
+
+print(func1())
