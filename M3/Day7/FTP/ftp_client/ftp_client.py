25afb40c1a9d7330f522bcdeec94be03dadcb29b
Zhang Qing <qzhang@i9i8.com>
2018-12-07 18:20:07 +0800

    2018-12-07

--- M3/Day7/FTP/ftp_client/ftp_client.py
+++ M3/Day7/FTP/ftp_client/ftp_client.py
@@ -1,3 +1,17 @@
 #_*_coding:utf-8_*_
 #__author__: Zhang Qing
 
+import socket
+import optparse
+
+class FTPClient(object):
+    def __init__(self):
+        parser=optparse.OptionParser()
+        parser.add_option("-s","--server",dest="server",help="ftp server IP")
+        parser.add_option("-P","--port",type="int",dest="port",help="ftp server port")
+        parser.add_option("-u","--user",dest="username",help="username")
+        parser.add_option("-p","--password",dest="password",help="password")
+        self.options, self.args = parser.parse_args()
+
+if __name__ == "__main__":
+    ftp = FTPClient()
